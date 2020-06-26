50f4c85d84e0252a344abb8189abe2d415f7ee9c
ValneiPereira <valnney@yahoo.com.br>
2020-06-26 16:39:37 -0300

    Projeto criado

---
+++ arithmetic_io.rb
@@ -0,0 +1,17 @@
+print "Digite o primeiro número inteiro: "
+# .to_i transforma a string em um número inteiro
+number1 = gets.chomp.to_i
+print "Digite o segundo número inteiro: "
+number2 = gets.chomp.to_i
+ 
+addition = number1 + number2
+puts "O resultado da adição entre os dois números é #{addition}"
+
+subtracao = number1 - number2
+puts "O resultado da subtração entre os dois números é #{subtracao}"
+
+divisao = number1 / number2
+puts "O resultado da divisão entre os dois números é #{divisao}"
+
+multiplica = number1 * number2
+puts "O resultado da multipliação entre os dois números é #{multiplica}"
---
+++ pessoa.rb
@@ -0,0 +1,24 @@
+# Crie um programa que recebe o nome e idade
+# de uma pessoa e nome e idade de uma pessoa
+#  e no final exiba estes dois dados em uma unica frase
+
+
+printf "======Dados de Cadastro======\n"
+
+t = Time.now
+
+print "Digite seu nome:"
+nome= gets.chomp
+
+print "Digite seu sobre nome:"
+sobre_nome= gets.chomp
+
+print "Digite seu ano de  nascimento:"
+ano_nascimento= gets.chomp.to_i
+
+ano_atual= t.strftime("%Y").to_i
+idade = ano_atual - ano_nascimento
+
+puts "Dados Cadastrais são:"
+puts "Nome :#{nome} #{sobre_nome} com idade de #{idade} anos!" 
+

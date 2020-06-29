d163a5bccf563e3126861aa990473a3d6b57945c
ValneiPereira <valnney@yahoo.com.br>
2020-06-29 11:33:45 -0300

    ruby aplicacao idade

--- Conceitos Básicos/hello.rb
+++ Conceitos Básicos/hello.rb
@@ -1,12 +1,12 @@
 # saída de dado
-print 'Digite seu nome: '
- 
+print "Digite seu nome: "
+
 # Recebendo uma entrada do teclado
 name = gets.chomp
 
-print 'Digite Sobrenome: '
+print "Digite Sobrenome: "
 sobrename = gets.chomp
- 
+
 # saída utilizando puts
 # use códigos ruby dentro de uma string com #{code}
-puts "Hello #{name} #{sobrename}!"   
+puts "Hello #{name} #{sobrename}!"
--- Conceitos Básicos/pessoa.rb
+++ Conceitos Básicos/pessoa.rb
@@ -2,23 +2,21 @@
 # de uma pessoa e nome e idade de uma pessoa
 #  e no final exiba estes dois dados em uma unica frase
 
-
 printf "======Dados de Cadastro======\n"
 
 t = Time.now
 
 print "Digite seu nome:"
-nome= gets.chomp
+nome = gets.chomp
 
 print "Digite seu sobre nome:"
-sobre_nome= gets.chomp
+sobre_nome = gets.chomp
 
 print "Digite seu ano de  nascimento:"
-ano_nascimento= gets.chomp.to_i
+ano_nascimento = gets.chomp.to_i
 
-ano_atual= t.strftime("%Y").to_i
+ano_atual = t.strftime("%Y").to_i
 idade = ano_atual - ano_nascimento
 
 puts "Dados Cadastrais são:"
-puts "Nome :#{nome} #{sobre_nome} com idade de #{idade} anos!" 
-
+puts "Nome :#{nome} #{sobre_nome} com idade de #{idade} anos!"

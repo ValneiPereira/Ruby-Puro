bfb3892698864cc03b2c63b7d78e02020552ad59
ValneiPereira <valnney@yahoo.com.br>
2020-06-29 09:39:53 -0300

    estruturas de controle

--- else.rb
+++ else.rb
@@ -1,9 +1,11 @@
-day = 'saturday' 
+day = 'Hollyday' 
 
 if day == 'Sunday' 
   lunch = 'special'
+elsif day =='Hollyday'
+  lunch = 'later'
 else
-  lunch = 'normal'    
+  lunch ='normal'
 end
 
 puts "Lunch is #{lunch} today"

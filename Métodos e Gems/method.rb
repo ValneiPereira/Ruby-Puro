7f955c4960ace1fb45bfe2d3bacf8234b38b1111
ValneiPereira <valnney@yahoo.com.br>
2020-06-29 18:18:10 -0300

    metodos e gems

---
+++ Métodos e Gems/method.rb
@@ -0,0 +1,17 @@
+# def talk(first_name, last_name)
+#   puts "Olá #{first_name} #{last_name}, como você está?"
+# end
+
+# first_name = "Valnei"
+# last_name = "Pereira"
+
+# talk(first_name, last_name)
+
+def signal(color = "vermelho")
+  puts "O sinal está #{color}"
+end
+
+signal
+
+color = "verde"
+signal(color)

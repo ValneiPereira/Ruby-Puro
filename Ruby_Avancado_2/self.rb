50259c7ebcdef42d8ec645da718ff51bcc5b81f6
ValneiPereira <valnney@yahoo.com.br>
2020-07-01 18:21:45 -0300

    protected

--- Ruby_Avancado_2/self.rb
+++ Ruby_Avancado_2/self.rb
@@ -6,30 +6,14 @@
 #   end
 # end
 
-# foo = Foo.new
-# puts foo
-# foo.bar
-
-# class Foo
-#   def self.bar
-#     puts self
-#   end
-# end
-
-# Foo.bar
+foo = Foo.new
+puts foo
+foo.bar
 
 class Foo
-  def call_private
-    bar
-  end
-
-  private
-
-  def bar
-    puts "private method"
+  def self.bar
+    puts self
   end
 end
 
-foo = Foo.new
-
-foo.call_private
+Foo.bar
